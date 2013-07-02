ce08f9668ae7c85f0c1b10e36d4ac18878407170
Jeff Larkin <jlarkin@nvidia.com>
2013-07-02 14:38:52 -0400

    Updated stuff

---
+++ saxpy_openacc_c.c
@@ -0,0 +1,22 @@
+void saxpy(int n, float a, float * restrict x, float * restrict y)
+{
+  #pragma acc kernels deviceptr(x,y)
+//  #pragma acc kernels deviceptr(x[0:n],y[0:n])
+  {
+    for(int i=0; i<n; i++)
+    {
+      y[i] += a*x[i];
+    }
+  }
+}
+void set(int n, float val, float * restrict arr)
+{
+#pragma acc kernels deviceptr(arr)
+//#pragma acc kernels deviceptr(arr[0:n])
+  {
+    for(int i=0; i<n; i++)
+    {
+      arr[i] = val;
+    }
+  }
+}

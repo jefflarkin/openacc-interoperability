995773e1dc6783449c1fc9a35c9e8c5708d47dc7
Jeff Larkin <jlarkin@nvidia.com>
2014-08-15 13:49:18 -0400

    Fixed broken example

--- openacc_cublas.f90
+++ openacc_cublas.f90
@@ -3,13 +3,16 @@ program main
   integer, parameter :: N = 2**20
   real, dimension(N) :: X, Y
 
+  !$acc data create(x,y)
+  !$acc kernels
   X(:) = 1.0
   Y(:) = 0.0
+  !$acc end kernels
 
-  !$acc data copy(y) copyin(x)
   !$acc host_data use_device(x,y)
   call cublassaxpy(N, 2.0, x, 1, y, 1)
   !$acc end host_data
+  !$acc update self(y)
   !$acc end data
 
   print *, y(1)

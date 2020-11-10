17e9515db77d3c1fb92331b8e3d8ee482ce0c734
Jeff Larkin <jeff.larkin@gmail.com>
2020-11-10 14:01:16 -0500

    Added long-missing "loop" directive

--- saxpy_openacc_f.f90
+++ saxpy_openacc_f.f90
@@ -3,7 +3,7 @@ module saxpy_mod
   subroutine saxpy(n, a, x, y)
     integer :: n
     real    :: a, x(:), y(:)
-    !$acc parallel deviceptr(x,y)
+    !$acc parallel loop deviceptr(x,y)
     y(:) = y(:) + a * x(:)
     !$acc end parallel
   end subroutine
